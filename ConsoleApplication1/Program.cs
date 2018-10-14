9aafdc9b112ffbeef70272ddc7bcf1baa8d6b297
Damon Shetty <damon_53@hotmail.com>
2018-10-14 17:04:07 +0100

    Commit simple file search

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FileSearch;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the directory");
+            string strdir = Console.ReadLine();
+
+            File fileobj = new File();
+            fileobj.sendFileName += DisplayFileName;
+
+
+            Thread thread = new Thread(() => 
+                fileobj.Search(strdir));
+            thread.Start();
+            Console.Read();
+        }
+
+
+        static void DisplayFileName(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
